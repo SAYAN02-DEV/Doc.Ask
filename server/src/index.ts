7538bcbd425240574b51302711cbebdfc7710a63
SAYAN02-DEV <sayan2003.dev@gmail.com>
2025-07-26 07:13:04 +0530

    aws'

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,8 @@ import cors from 'cors';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
+
 
 app.use(express.json());
 app.use(cors({
@@ -14,7 +15,11 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'token'],
   credentials: false,
 }));
+import userRoutes from './routes/userRoutes';
+import uploadRoute from './routes/uploadRoute';
 
+app.use('/user', userRoutes);
+app.use('/api', uploadRoute)
 const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/doc_ask';
 if (!mongoUri) {
   throw new Error('MONGO_URI environment variable not set');
@@ -22,7 +27,7 @@ if (!mongoUri) {
 mongoose.connect(mongoUri)
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(PORT, () => {
+    app.listen(PORT,'0.0.0.0',() => {
       console.log(`Server running on http://localhost:${PORT}`);
     });
   })
@@ -31,8 +36,3 @@ mongoose.connect(mongoUri)
   });
 
 
-import userRoutes from './routes/userRoutes';
-import uploadRoute from './routes/uploadRoute';
-
-app.use('/user', userRoutes);
-app.use('/api', uploadRoute)
