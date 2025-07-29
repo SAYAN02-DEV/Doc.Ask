7afe3f423daa647fc85e43f3bdf29b9fc043f3b5
SAYAN02-DEV <sayan2003.dev@gmail.com>
2025-07-29 11:36:01 +0530

    dep2

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'token'],
   credentials: false,
